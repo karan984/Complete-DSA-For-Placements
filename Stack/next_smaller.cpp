3e45981d48a92f749f3f81993b085afa4e0f6623
Karan Mashru <mashru159@gmail.com>
2025-01-11 21:49:22 +0530

    Next Smaller Element, Previous Smaller Element, Previous Greater Element

---
+++ Stack/next_smaller.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main()
+{
+    int n, i, j;
+    stack<int> s;
+
+    cin>>n;
+    int a[n], ans[n];
+
+    for(i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+
+    for(i=n-1; i>=0; i--)
+    {
+        while(s.empty()==false && s.top()>=a[i]){
+            s.pop();
+        }
+
+        if(s.empty()==false){
+            ans[i]=s.top();
+        }else{
+            ans[i]=-1;
+        }
+
+        s.push(a[i]);
+    }
+
+    for(i=0; i<n; i++)
+    {
+        cout<<ans[i]<<" ";
+    }
+}
